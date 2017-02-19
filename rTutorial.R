b332d4be0b00c0a8451e17904257fdd06fe8c91f
jotasolano <jsolano91@gmail.com>
2017-02-18 23:48:42 -0500

    :rocket: First commit

---
+++ rTutorial.R
@@ -0,0 +1,21 @@
+# The r environment
+# - how can r be used
+# - custom interface
+
+
+# Object types
+# - numeric
+# - character
+# - logical
+
+
+# Data structures and cohersion
+# - vector and matrix
+# - data.frame
+
+
+# Importing csv
+
+# Selections in a data frame
+# - by number
+# - by name
